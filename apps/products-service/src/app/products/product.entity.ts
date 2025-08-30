6fc7d2006120f7dc00c79d6efa9c49d88ebe1d5c
wwysopal <wioletta.wysopal@gmail.com>
2025-08-30 09:11:59 +0200

    WW-5 TASK 2.1: Implementacja modelu danych Product

--- apps/products-service/src/app/products/product.entity.ts
+++ apps/products-service/src/app/products/product.entity.ts
@@ -1,7 +1,7 @@
 export class Product {
-  id: number;
-  name: string;
-  price: number;
-  quantity: number;
+  id!: number;
+  name!: string;
+  price!: number;
+  quantity!: number;
   description?: string;
 }
