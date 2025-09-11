0b4238b0370f111aced0ea0a88e501bc1c1bfcab
wwysopal <wioletta.wysopal@gmail.com>
2025-09-11 08:10:15 +0200

    WW-36 TASK 6.9: Implementacja Prisma Service dla Orders

---
+++ apps/orders-service/src/app/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+// apps/orders-service/src/app/prisma/prisma.service.ts
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'file:./apps/orders-service/prisma/orders.db',
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
