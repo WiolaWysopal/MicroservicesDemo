75cf57cd23072e340d200766b2af0c7c726fee8d
Bartlomiej Zimny <bartekziimny90@gmail.com>
2025-10-22 00:44:32 +0200

    Refactor ProductsModule by removing HttpModule and PrismaModule imports, and eliminate duplicate PrismaService from providers.

--- apps/products-service/src/app/products/products.module.ts
+++ apps/products-service/src/app/products/products.module.ts
@@ -2,20 +2,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { PrismaService } from '../prisma/prisma.service';
-import {HttpModule} from "@nestjs/axios";
-import {PrismaModule} from "../prisma/prisma.module";
 
 @Module({
-  imports: [
-    HttpModule.register({
-      timeout: 5000,
-      maxRedirects: 5,
-    }),
-    PrismaModule, // <- dodaj tutaj
-  ],
   controllers: [ProductsController],
-  providers: [ProductsService, PrismaService, PrismaService],
+  providers: [ProductsService],
   exports: [ProductsService],
 })
 export class ProductsModule {}
+
