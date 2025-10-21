36ab2b523951e94d20781cb0e262e0bca5298b9f
Bartlomiej Zimny <bartekziimny90@gmail.com>
2025-10-22 00:35:30 +0200

    Enhance ProductsModule by adding HttpModule with timeout and maxRedirects settings, and include PrismaModule in imports. Corrected duplicate PrismaService in providers.

--- apps/products-service/src/app/products/products.module.ts
+++ apps/products-service/src/app/products/products.module.ts
@@ -3,10 +3,19 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { PrismaService } from '../prisma/prisma.service';
+import {HttpModule} from "@nestjs/axios";
+import {PrismaModule} from "../prisma/prisma.module";
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    PrismaModule, // <- dodaj tutaj
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService, PrismaService],
+  providers: [ProductsService, PrismaService, PrismaService],
   exports: [ProductsService],
 })
 export class ProductsModule {}
